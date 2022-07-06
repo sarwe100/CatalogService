fc0b2ebd65f62f6069834372f1f9eec8453e6ccb
krushnakumar.sarwe <krushnakumar.sarwe@KRUSHNAKUMAR_I5>
2022-07-06 12:19:21 +0530

    done

--- Catalog.API/Controllers/CatalogController.cs
+++ Catalog.API/Controllers/CatalogController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class CatalogController : ControllerBase
     {
