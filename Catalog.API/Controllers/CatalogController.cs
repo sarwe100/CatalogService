be1b58f082d1ce7ee4809a48987b1a6afed7f153
krushnakumar.sarwe <krushnakumar.sarwe@KRUSHNAKUMAR_I5>
2022-07-07 09:40:49 +0530

    save changes

--- Catalog.API/Controllers/CatalogController.cs
+++ Catalog.API/Controllers/CatalogController.cs
@@ -67,7 +67,7 @@ namespace Catalog.API.Controllers
             var items = await _repository.GetProductByName(name);
             if (items == null)
             {
-                _logger.LogError($"Products with name: {name} not found.");
+                _logger.LogError($"Products with name: {name} not found in data.");
                 return NotFound();
             }
             return Ok(items);
